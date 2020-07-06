9e57e35b3688cc282046e4d3e338db96db7a8716
irfan hadian <irfan.hadian10@gmail.com>
2020-07-06 12:46:11 +0700

    add todo route api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('todos', 'TodoController@index');
+Route::post('todo', 'TodoController@store');
+Route::get('todo/{todo}', 'TodoController@show');
+Route::put('todo/{todo}', 'TodoController@update');
+Route::delete('todo/{todo}', 'TodoController@destroy');
