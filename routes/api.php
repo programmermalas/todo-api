52a30cbc912b3af1339a28debc962daf57241808
irfan hadian <irfan.hadian10@gmail.com>
2020-07-06 13:50:33 +0700

    fixed todo route api

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('todos', 'TodoController@index');
-Route::post('todo', 'TodoController@store');
-Route::get('todo/{todo}', 'TodoController@show');
-Route::put('todo/{todo}', 'TodoController@update');
-Route::delete('todo/{todo}', 'TodoController@destroy');
+Route::get('todos', 'API\TodoController@index');
+Route::post('todo', 'API\TodoController@store');
+Route::get('todo/{todo}', 'API\TodoController@show');
+Route::put('todo/{todo}', 'API\TodoController@update');
+Route::delete('todo/{todo}', 'API\TodoController@destroy');
